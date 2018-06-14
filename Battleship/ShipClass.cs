ecf5632050c58a8b9cb62ff73e9e74f190934fef
Paul Krug <paul.krug@changepoint.com>
2018-06-14 14:01:46 -0700

    Add Fleet and Ship objects

---
+++ Battleship/ShipClass.cs
@@ -0,0 +1,12 @@
+namespace Battleship
+{
+    public enum ShipClass
+    {
+        Unknown = 0,
+        Destroyer = 1,
+        Submarine = 2,
+        Cruiser = 3,
+        Battleship = 4,
+        Carrier = 5
+    }
+}
