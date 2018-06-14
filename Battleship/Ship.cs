f784e0e2971f187ad5730fbbdb2c1b7be21c7336
Paul Krug <paul.krug@changepoint.com>
2018-06-14 14:57:20 -0700

    Rework fleet and ship

--- Battleship/Ship.cs
+++ Battleship/Ship.cs
@@ -2,8 +2,29 @@ namespace Battleship
 {
     public class Ship
     {
-        public static ShipClass ShipClassThis {get; set;}   
+        public static ShipClass ShipClass {get; set;}   
 
-        public bool[] ShipDamage = new bool[Fleet.ShipSize[ShipClassThis]];
+        public ShipLocation Position {get; set;}
+
+        public bool[] ShipDamage = new bool[ShipClass];
+    }
+
+    public struct ShipLocation
+    {
+        public int[] Coordinates;
+        public Direction Orientation;
+    }
+
+    public Dictionary<ShipClass,int> ShipSize = new Dictionary<ShipClass,int>
+    {
+        {Destroyer, 2}
+    };
+
+    public enum Direction
+    {
+        North = 0,
+        East = 1,
+        South = 2,
+        West = 3
     }
 }
