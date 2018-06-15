871fee86bb6740912fd5abd780b9afc85a5e2116
Jared Reynolds <jared.reynolds@changepoint.com>
2018-06-14 17:53:05 -0700

    Cleanup/reformat Game

--- Battleship/Game.cs
+++ Battleship/Game.cs
@@ -1,44 +1,38 @@
 using System;
 using System.Linq;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Blazor.Services;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Battleship
 {
     public class Game
     {
-        // 2 players
-        // each player has their board
-        // will need a player class
-        // there are 2 boards in play - the ui will differ depending on who is viewing it
-        // need to be able to disable clicking on board not in play
-
-        // private static Game _game;
-        // private Game() {}
-        // public static Game Instance{ get{
-        //     return _game ?? (_game = new Game());
-        // }}
-        private IUriHelper _uriHelper;
-        public Game(IUriHelper uriHelper){
-            _uriHelper = uriHelper;
-        }
-
-        public Player[] players = new []{
+        private readonly IUriHelper _uriHelper;
+        private readonly Player[] _players = new[]{
             new Player(0),
             new Player(1)
         };
 
-        public Players Players{ get{
-            var me = GetPlayerId();
-            
-            return new Players(players[me], players[me == 0 ? 1 : 0]);
-        }}
+        public Game(IUriHelper uriHelper)
+        {
+            _uriHelper = uriHelper;
+        }
 
-        public int Turn{ get; set; }
+        public Players Players
+        {
+            get
+            {
+                var me = GetPlayerId();
+                return new Players(_players[me], _players[me == 0 ? 1 : 0]);
+            }
+        }
 
-        private int GetPlayerId() {
+        public int Turn { get; set; }
+
+        private int GetPlayerId()
+        {
             var uri = new Uri(_uriHelper.GetAbsoluteUri());
-            return Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query).TryGetValue("id", out var id) ? int.Parse(id.First()) : 0;
+            return QueryHelpers.ParseQuery(uri.Query).TryGetValue("id", out var id) ? int.Parse(id.First()) : 0;
         }
     }
 }
